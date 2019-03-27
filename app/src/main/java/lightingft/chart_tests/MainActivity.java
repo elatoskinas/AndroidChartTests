bb359272ec5b4d0c15227d601a0b653f88cc0560
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-03-27 19:26:22 +0100

    Add test LineChart

--- app/src/main/java/lightingft/chart_tests/MainActivity.java
+++ app/src/main/java/lightingft/chart_tests/MainActivity.java
@@ -2,11 +2,17 @@ package lightingft.chart_tests;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout chartLayout = findViewById(R.id.chartLayout);
+
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        for (int i = 1; i <= 10; ++i) {
+            Entry entry = new Entry(i, i);
+            entries.add(entry);
+        }
+
+        DataSet<Entry> dataSet = new LineDataSet(entries, "Entries");
+        LineData lineData = new LineData((LineDataSet) dataSet);
+        LineChart lineChart = new LineChart(this);
+        lineChart.setData(lineData);
+
+        lineChart.setMinimumHeight(500);
+
+        chartLayout.addView(lineChart);
     }
 }
