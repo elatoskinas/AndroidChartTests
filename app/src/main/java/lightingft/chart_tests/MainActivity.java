66851d0103f540115a36b87f9320fa9eb79f0858
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-03-27 18:24:51 +0100

    Fix build error; Add tests chart

--- app/src/main/java/lightingft/chart_tests/MainActivity.java
+++ app/src/main/java/lightingft/chart_tests/MainActivity.java
@@ -3,6 +3,11 @@ package lightingft.chart_tests;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
