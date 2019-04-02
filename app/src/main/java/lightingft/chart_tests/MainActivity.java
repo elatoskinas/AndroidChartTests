12bb19c00689a9539f388e9488c369aeb5c299d2
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-04-02 22:34:21 +0200

    Add BubbleChart & RadarChart examples

--- app/src/main/java/lightingft/chart_tests/MainActivity.java
+++ app/src/main/java/lightingft/chart_tests/MainActivity.java
@@ -6,14 +6,19 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.BubbleChart;
 import com.github.mikephil.charting.charts.CandleStickChart;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.charts.RadarChart;
 import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.BubbleData;
+import com.github.mikephil.charting.data.BubbleDataSet;
+import com.github.mikephil.charting.data.BubbleEntry;
 import com.github.mikephil.charting.data.CandleData;
 import com.github.mikephil.charting.data.CandleDataSet;
 import com.github.mikephil.charting.data.CandleEntry;
@@ -25,6 +30,9 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
 import com.github.mikephil.charting.data.ScatterData;
 import com.github.mikephil.charting.data.ScatterDataSet;
 
@@ -43,11 +51,15 @@ public class MainActivity extends AppCompatActivity {
         BarChart barChart = createBarChart();
         PieChart pieChart = createPieChart();
         ScatterChart scatterChart = createScatterChart();
+        BubbleChart bubbleChart = createBubbleChart();
+        RadarChart radarChart = createRadarChart();
 
         chartLayout.addView(lineChart);
         chartLayout.addView(barChart);
         chartLayout.addView(pieChart);
         chartLayout.addView(scatterChart);
+        chartLayout.addView(bubbleChart);
+        chartLayout.addView(radarChart);
     }
 
     /**.
@@ -67,6 +79,10 @@ public class MainActivity extends AppCompatActivity {
             dataSet = new PieDataSet(new ArrayList<PieEntry>(), "Entries");
         else if (data instanceof ScatterData)
             dataSet = new ScatterDataSet(new ArrayList<Entry>(), "Entries");
+        else if (data instanceof BubbleData)
+            dataSet = new BubbleDataSet(new ArrayList<BubbleEntry>(), "Entries");
+        else if (data instanceof RadarData)
+            dataSet = new RadarDataSet(new ArrayList<RadarEntry>(), "Entries");
 
         populateDataSet(dataSet, scalar);
         dataSet.setColors(colors);
@@ -121,7 +137,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Create new PieData Object and populate it
         PieData pieData = new PieData();
-        addDataSetToChartData(pieData, 1, Color.YELLOW, Color.CYAN, Color.RED);
+        addDataSetToChartData(pieData, 1, Color.RED,Color.GREEN,
+                Color.BLUE, Color.CYAN, Color.YELLOW, Color.MAGENTA, Color.GRAY, Color.BLACK,
+                Color.DKGRAY, Color.LTGRAY);
+
+        pieChart.setDrawEntryLabels(false);
 
         // Set PieChart data to PieData object
         pieChart.setData(pieData);
@@ -154,6 +174,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**.
+     * Creates a sample BubbleChart
+     * @return - sample BubbleChart object with test data
+     */
+    public BubbleChart createBubbleChart() {
+        // Create new BubbleChart with empty BubbleData object
+        BubbleChart bubbleChart = new BubbleChart(this);
+        bubbleChart.setData(new BubbleData());
+
+        // Populate BubbleData
+        addDataSetToChartData(bubbleChart.getData(), 1, Color.MAGENTA);
+
+        // Set Chart minimum height
+        bubbleChart.setMinimumHeight(500);
+
+        return bubbleChart;
+    }
+
+    /**.
+     * Creates a sample RadarChart
+     * @return - sample RadarChart object with test data
+     */
+    public RadarChart createRadarChart() {
+        // Create new RadarChart with empty RadarData object
+        RadarChart radarChart = new RadarChart(this);
+
+        RadarData radarData = new RadarData();
+
+        // Populate RadarData
+        addDataSetToChartData(radarData, 1, Color.BLUE);
+
+        radarChart.setData(radarData);
+
+        // Set Chart minimum height
+        radarChart.setMinimumHeight(500);
+
+        return radarChart;
+    }
+
+    /**.
      * Populates the provided Data Set with test values
      * @param dataSet - dataSet to populate
      */
@@ -169,7 +228,11 @@ public class MainActivity extends AppCompatActivity {
             if (dataSet instanceof BarDataSet)
                 entry = new BarEntry(i, scalar*i);
             else if (dataSet instanceof PieDataSet)
-                entry = new PieEntry(i, scalar*i);
+                entry = new PieEntry(scalar*i, "#" + i);
+            else if (dataSet instanceof RadarDataSet)
+                entry = new RadarEntry(i, scalar*i);
+            else if (dataSet instanceof BubbleDataSet)
+                entry = new BubbleEntry(i*1.0f, i*1.0f, scalar*i*0.5f);
             else
                 entry = new Entry(i, scalar*i);
 
