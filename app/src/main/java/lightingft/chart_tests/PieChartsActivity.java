c4d22a69eb8a86637c6ca8d9820e49f157779de1
Evaldas Latoskinas <evaldasl61@gmail.com>
2019-06-23 01:32:19 +0200

    Add dynamic ring adding functionality to Pie Charts Activity

--- app/src/main/java/lightingft/chart_tests/PieChartsActivity.java
+++ app/src/main/java/lightingft/chart_tests/PieChartsActivity.java
@@ -3,6 +3,8 @@ package lightingft.chart_tests;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -18,22 +20,31 @@ import java.util.Random;
 public class PieChartsActivity extends AppCompatActivity {
 
     PieChart mainPieChart;
-
-    List<PieChart> innerPieCharts;
-
+    List<PieChart> charts;
     RelativeLayout chartLayout;
+
+    Button addPieChartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pie_charts);
 
+        charts = new ArrayList<PieChart>();
+
         chartLayout = findViewById(R.id.pieChartLayout);
 
         initializeMainPieChart();
         chartLayout.addView(mainPieChart);
 
-        addInnerPieChart();
+        addPieChartButton = findViewById(R.id.addInnerPieChartButton);
+
+        addPieChartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addInnerPieChart();
+            }
+        });
     }
 
     private void initializeMainPieChart() {
@@ -41,12 +52,23 @@ public class PieChartsActivity extends AppCompatActivity {
 
         mainPieChart.setMinimumHeight(1000);
         mainPieChart.setMinimumWidth(1000);
+
+        charts.add(mainPieChart);
     }
 
     private void addInnerPieChart() {
+        PieChart lastChart = charts.get(charts.size() - 1);
+
+        lastChart.setHoleRadius(75f);
+        lastChart.setTransparentCircleRadius(75f);
+        lastChart.invalidate();
+
+        System.out.println(lastChart.getMinimumWidth());
+        int width = (int)(lastChart.getMinimumWidth() * 0.75f);
+
         PieChart innerChart = createPieChart();
-        innerChart.setMinimumHeight(700);
-        innerChart.setMinimumWidth(700);
+        innerChart.setMinimumHeight(width);
+        innerChart.setMinimumWidth(width);
 
         chartLayout.addView(innerChart);
 
@@ -54,6 +76,8 @@ public class PieChartsActivity extends AppCompatActivity {
                 .getLayoutParams();
         params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
         innerChart.setLayoutParams(params);
+
+        charts.add(innerChart);
     }
 
     private PieChart createPieChart() {
